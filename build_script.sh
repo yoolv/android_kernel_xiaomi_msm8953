6b805479b5df96edcf2cab133faaa6d778b5ac67
yoolv <chaelsora0@gmail.com>
2022-06-23 21:11:31 +0700

    Treble Support

---
+++ build_script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export ARCH=arm64
+export SUBARCH=arm64
+export HEADER_ARCH=arm64
+export PATH="${HOME}/miinguri/Documents/Kernel/proton-clang-master/bin:${PATH}"
+export STRIP="${HOME}/miinguri/Documents/Kernel/proton-clang-master/bin/strip"
+
+if [[ -d "out" ]]
+then
+cd out && make clean && make distclean && make mrproper && cd ..
+else
+    mkdir -p out
+    fi
+
+    make O=out ARCH=arm64 tissot_defconfig
+
+make -j$(nproc --all) O=out \
+		ARCH=arm64 \
+		AR=llvm-ar \
+		NM=llvm-nm \
+		OBJCOPY=llvm-objcopy \
+		OBJDUMP=llvm-objdump \
+		STRIP=llvm-strip \
+		CC=clang \
+		CROSS_COMPILE=aarch64-linux-gnu- \
+		CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+                       
+
